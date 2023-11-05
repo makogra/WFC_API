b829a5153f2c8f4c7af27c5eb8b60db0c62286b4
makogra <pietrzak.maciek.pv@gmail.com>
2023-11-05 07:09:24 +0100

    init project

---
+++ src/main/java/com/mako/spring/wfc/wfcapi/WfcapiApplication.java
@@ -0,0 +1,13 @@
+package com.mako.spring.wfc.wfcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfcapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfcapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mako/spring/wfc/wfcapi/WfcapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mako.spring.wfc.wfcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfcapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
